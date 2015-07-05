340da37772f8befa433acd2ed202879602b78af8
Salas <gae.m.project@gmail.com>
2015-07-05 21:40:34 +0800

    dict the result

--- train.py
+++ train.py
@@ -15,35 +15,27 @@ def get_results(url, params):
     response = requests.get(url, params=params)
     document = lxml.html.fromstring(response.text)
     for row in document.xpath("//tbody/tr[@class='Grid_Row']"):
-        train_type = row.xpath("./td[1]//div/span")[0]
-        train_code = row.xpath("./td[2]//a")[0]
-        versa = row.xpath("./td[3]/font")[0]
-        route = row.xpath("./td[4]/font")[0]
-        launch_time = row.xpath("./td[5]/font")[0]
-        arrive_time = row.xpath("./td[6]/font")[0]
-        duration = row.xpath("./td[7]/font")[0]
-        notes = row.xpath("./td[8]//div/img")
-        comment = row.xpath("./td[8]//div/span[@id='Comment']")[0]
-        price = row.xpath("./td[9]//span")[0]
-        notes = list_it(notes, comment.text)
+        r = {}
+        r['train_type'] = row.xpath("./td[1]//div/span")[0].text
+        r['train_type'] = row.xpath("./td[1]//div/span")[0].text
+        r['train_code'] = row.xpath("./td[2]//a")[0].text
+        r['versa'] = row.xpath("./td[3]/font")[0].text
+        r['route'] = row.xpath("./td[4]/font")[0].text
+        r['launch_time'] = row.xpath("./td[5]/font")[0].text
+        r['arrive_time'] = row.xpath("./td[6]/font")[0].text
+        r['duration'] = row.xpath("./td[7]/font")[0].text
+        r['comment'] = row.xpath("./td[8]//div/span[@id='Comment']")[0].text
+        r['price'] = row.xpath("./td[9]//span")[0].text
+        # r['notes'] = row.xpath("./td[8]//div/img")
+        # r['notes'] = list_it(notes, comment.text)
 
-        results.append({
-            'train_type': train_type.text,
-            'train_code': train_code.text,
-            'versa': versa.text,
-            'route': route.text,
-            'launch_time': launch_time.text,
-            'arrive_time': arrive_time.text,
-            'duration': duration.text,
-            'price': price.text,
-            'notes': notes
-        })
+        results.append(r)
     return results
 
 if __name__ == '__main__':
     params = {
         'searchtype': 0,
-        'searchdate': '2015/06/07',
+        'searchdate': '2015/07/07',
         'fromstation': 1317,
         'tostation': 1025,
         'trainclass': 2,
