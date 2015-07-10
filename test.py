9e615da45bdd13f3b49b82d5d14976d6adf01fe8
leVirve <gae.m.project@gmail.com>
2015-07-11 00:57:13 +0800

    add name-to-code options

--- test.py
+++ test.py
@@ -4,10 +4,20 @@ if __name__ == '__main__':
 
     timetable = tawin_rails_system.TrainTimetable()
     results = timetable.query(
+        name_code=False,
         searchtype=0,
-        searchdate='2015/07/09',
+        searchdate='2015/07/11',
         fromstation=1025,  # 豐原
         tostation=1317,  # 新竹
+        trainclass=2,  # 對號/無對號/全部
+        fromtime='0000',  # 00:00
+        totime='2359',  # 23:59
+    )
+    results = timetable.query(
+        searchtype=0,
+        searchdate='2015/07/11',
+        fromstation='豐原',  # 豐原
+        tostation='新竹',  # 新竹
         trainclass=2,  # 對號/無對號/全部
         fromtime='0000',  # 00:00
         totime='2359',  # 23:59
@@ -16,3 +26,5 @@ if __name__ == '__main__':
     import pprint
     for r in results:
         pprint.pprint(r)
+
+    print(timetable.station_name_to_code('豐原'))
